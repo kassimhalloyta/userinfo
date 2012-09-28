e362a20243a6a52ac0b7ca7d2fd0fdc53198f145
Kassim Halloyta <kassim@newish.dk>
2012-09-29 01:16:29 +0200

    User info project is created

---
+++ user/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+User::Application.config.secret_token = '7b6b1a3bda4ec187273e02598a1f9048a0f2117c394eef711bd9bb6f15c0d360adc1d3dece3cc1a8c0120a1da9e45b6c982bea91b77ad7ce4253199528abc49a'
